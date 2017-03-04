51e645d5dba071c83dd0467f9bebd2d79c0bfe94
Bertan Atalay <bertan@2creative.net>
2017-03-04 17:44:58 +0000

    updating the navbar with the menu button placeholder

--- src/repo_components/navbar.js
+++ src/repo_components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Icon from '../components/Icons/Icon'
 import './navbar.css'
 
 const RepoNavbar = () => {
@@ -7,6 +8,12 @@ const RepoNavbar = () => {
       <div className="container">
         <div className="navbar-header">
           <a href="#" className="navbar-brand sui-logo">SUI-React</a>
+          <div className="btn-group bc-btn-navbar-group">
+            <button type="button" className="btn">
+              Components
+              <Icon name="menu" />
+            </button>
+          </div>
         </div>
       </div>
     </nav>  
